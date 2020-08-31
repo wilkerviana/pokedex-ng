19224a0f4714e74047fee00273b32e1266689094
Wilker Viana <wilker.info@gmail.com>
2020-08-31 16:42:31 -0300

    pokedex(base): Initial configs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PokedexComponent } from './modules/pokedex/pokedex.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/pokedex/pokedex.module').then((m) => m.PokedexModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
